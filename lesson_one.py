a67785cf027bbf07379fbeda3e18ce74c289c7da
Michael Bag <michaelbag8@gmail.com>
2026-07-21 20:31:22 +0100

    Add string to integer conversion and name concatenation

--- lesson_one.py
+++ lesson_one.py
@@ -1,4 +1,12 @@
 infinity= 2e400
 print(infinity)
 
+#string to integer
+age = "40"
+print(int(age))
 
+#concatenation
+first_name = "Damilo"
+last_name = "Gwarinpa"
+full_name = first_name + " " + last_name
+print(full_name)
