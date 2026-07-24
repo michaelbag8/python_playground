06469d0d8dcc215ea1cdc6cdb948843e747e33c4
Michael Bag <michaelbag8@gmail.com>
2026-07-24 01:07:53 +0100

    Implement substring functions and Person class
    Added functions for substring search and return, and defined a Person class with initialization.

--- lesson_one.py
+++ lesson_one.py
@@ -34,3 +34,36 @@ audio_clip.write_audiofile(mp3_file)
 os.remove(temp_file)
 
 print("Audio extracted and saved as MP3 file to", mp3_file)
+
+def substring_return(text, substring):
+    if substring in text:
+        return substring
+    return none
+
+def substring_search(text, target):
+    return target.lower() in text.lower()
+
+def search_name(str, target):
+    matches = []
+    for t in str:
+        if t == target:
+            matches.append(t)
+    return matches 
+
+class Person:
+   def __init__(self, name, age):
+        self.name =name
+        self.age = age
+        print(f"{name} is my name, and I am {age} years old")
+
+joe = Person ("Joe",46)
+sam = Person ("Samantha",10)
+
+sub =substring_search("hello my lovely lady", "lady")
+call = search_name("my name is joey", "n")
+print(call)
+print(sub)
+
+re = substring_return("hello duniya", "duniya")
+print(re)
+
