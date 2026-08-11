3280c098379c8472615657d1380afd84a85e5a72
Michael Bag <michaelbag8@gmail.com>
2026-08-11 22:39:02 +0100

    Add calculate_discount function with docstring
    Added a function to calculate discounted prices with an optional rounding feature and a docstring.

--- datacamp.py
+++ datacamp.py
@@ -114,3 +114,16 @@ def clean_text(text, lower=True):
         return clean_text.lower()
       
 print(help(clean_text))
+
+def calculate_discount(price, discount_percent=15, round_result=True):
+    # Add a single-line docstring
+    """Calculate the discounted price of a product."""
+    
+    discounted_price = price - (price * (discount_percent / 100))
+    if round_result == True:
+        return round(discounted_price, 2)
+    else:
+        return discounted_price
+
+# Access the docstring
+print(calculate_discount.__doc__)
