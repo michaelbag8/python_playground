398dccbc528fce633acf1c0098883e88c826a599
Michael Bag <michaelbag8@gmail.com>
2026-08-11 22:38:24 +0100

    Implement clean_text function with docstring
    Added a clean_text function to process strings.

--- datacamp.py
+++ datacamp.py
@@ -94,3 +94,23 @@ def test_report(durations):
 
 # Generate the report for recent test runs
 test_report(test_durations)
+
+def clean_text(text, lower=True):
+    # Add a multi-line docstring
+    """
+    Clean text by swapping spaces to underscores and converting to lowercase.
+    
+    Args:
+    	text (str): A string to be cleaned.
+    	lower (bool): Whether to convert the text to lowercase.
+    
+    Returns:
+    	text(str): Cleaned string.
+    """
+    clean_text = text.replace(' ', '_')
+    if lower == False:
+        return clean_text
+    else:
+        return clean_text.lower()
+      
+print(help(clean_text))
