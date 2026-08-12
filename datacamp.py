24fc9b38f0613a1be89d72666d227d6dbe9bc7b2
Michael Bag <michaelbag8@gmail.com>
2026-08-12 14:25:58 +0100

    Update datacamp.py

--- datacamp.py
+++ datacamp.py
@@ -1,6 +1,7 @@
 #DataCamp Python Programming Fundamentals 
 #Python for Developers
 import string 
+import pandas as pd
 total_confirmations = 10
 guest_count = 0
 
@@ -143,3 +144,30 @@ def calculate_discount(price, discount_percent=15, round_result=True):
 # Call the function with keyword arguments
 final_price = calculate_discount(price=original_price,discount_percent=25,round_result=False)
 print(final_price)
+
+# Define count_entries()
+def count_entries(csv_file, c_size, colname):
+    """Return a dictionary with counts of
+    occurrences as value for each key."""
+    
+    # Initialize an empty dictionary: counts_dict
+    counts_dict = {}
+
+    # Iterate over the file chunk by chunk
+    for chunk in pd.read_csv(csv_file, chunksize=c_size):
+
+        # Iterate over the column in DataFrame
+        for entry in chunk[colname]:
+            if entry in counts_dict.keys():
+                counts_dict[entry] += 1
+            else:
+                counts_dict[entry] = 1
+
+    # Return counts_dict
+    return counts_dict
+
+# Call count_entries(): result_counts
+result_counts = count_entries("tweets.csv", 10, "lang")
+
+# Print result_counts
+print(result_counts)
