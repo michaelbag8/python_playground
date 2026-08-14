929ca95c912233b23e99401e6f55961fb0629299
Michael Bag <michaelbag8@gmail.com>
2026-08-14 23:25:57 +0100

    implement attendance tracking logic into a function
    Implement attendance tracking into a function that calculates eligibility based on attendance percentage.

---
+++ py_tricks.py
@@ -0,0 +1,23 @@
+attendance = {
+    "Emma": ["P","P","A","P","P"],
+    "James": ["A","P","P","P","P"],
+    "Joel": ["P","A","A","P","P"],
+    "Desire": ["P","P","P","P","P"]
+}
+
+# refactor into a function
+for student, record in attendance.items():
+    present = record.count("P")
+    percentage = present / len(record) * 100
+
+
+    status = (
+        "Eligible"
+        if percentage >= 75
+        else "Not Eligible"
+        )
+
+print(student)
+print(f"Attendance: {percentage:.0f}%")
+print(status)
+print("-" * 20)
