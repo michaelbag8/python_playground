0b0bbb8a7e6e5e84e443f0179cb81352a548ae19
Michael Bag <michaelbag8@gmail.com>
2026-08-16 16:16:25 +0100

    Implement variable scope examples with functions
    Added functions to demonstrate variable scope and global usage.

--- funcs.py
+++ funcs.py
@@ -73,3 +73,20 @@ def read_files():
   return file_contents
 
 print('\n'.join(read_files()))
+
+x = 50
+
+def one():
+  x = 10
+
+def two():
+  global x
+  x = 30
+
+def three():
+  x = 100
+  print(x)
+
+for func in [one, two, three]:
+  func()
+  print(x)
