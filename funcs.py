6d880a7c73ec524395c5b73ef872d0e9d111ac81
Michael Bag <michaelbag8@gmail.com>
2026-08-17 13:58:51 +0100

    Implement build_tooltip function for docstrings
    Added a function to create tooltips displaying a function's docstring.

--- funcs.py
+++ funcs.py
@@ -192,3 +192,26 @@ undecorated_time = t_end - t_start
 
 print('Decorated time: {:.5f}s'.format(decorated_time))
 print('Undecorated time: {:.5f}s'.format(undecorated_time))
+
+
+#docstring
+import inspect
+
+def build_tooltip(function):
+  """Create a tooltip for any function that shows the
+  function's docstring.
+
+  Args:
+    function (callable): The function we want a tooltip for.
+
+  Returns:
+    str
+  """
+  # Get the docstring for the "function" argument by using inspect
+  docstring = inspect.getdoc(function)
+  border = '#' * 28
+  return '{}\n{}\n{}'.format(border, docstring, border)
+
+print(build_tooltip(count_letter))
+print(build_tooltip(range))
+print(build_tooltip(print))
