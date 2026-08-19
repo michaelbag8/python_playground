4b6a0e0696f2387959e706f23f6b5a24d5c985e0
Michael Bag <michaelbag8@gmail.com>
2026-08-19 11:45:44 +0100

    Enhance report_to_duty function with letter validation
    Added validation to ensure only letters are allowed in the name.

--- group_two.py
+++ group_two.py
@@ -1,12 +1,18 @@
 # group two task
 def report_to_duty(name):
+    #type check
     if not isinstance(name, str):
         return "Only string is allowed"
 
+    #removing trailing and leading space 
     name = name.strip()
+    #checking for empty value
     if not name:
         return "Name cannot be empty"
-    
+    #checking for only letters
+    if not name.replace(" ", "").isalpha():
+        return "Only letters are allowed"
+        
     return f"Recruit {name} reporting for duty"
 
 print(report_to_duty("    James    "))
