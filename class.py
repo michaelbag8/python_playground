2fac0fbb7910267e46898118b0d611c602de5780
Michael Bag <michaelbag8@gmail.com>
2026-08-24 08:43:36 +0100

    Add Person class with from_birth_year method

--- class.py
+++ class.py
@@ -127,3 +127,22 @@ xmas = BetterDate.from_str("2024-12-25")
 print(xmas.year)
 print(xmas.month)
 print(xmas.day)
+
+class Person:
+  CURRENT_YEAR = 2024
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+  
+  # Add a class method decorator
+  @classmethod
+  # Define the from_birth_year method
+  def from_birth_year(cls, name, birth_year):
+    # Create age
+    age = cls.CURRENT_YEAR - birth_year
+    # Return the name and age
+    return cls(name, age)
+
+bob = Person.from_birth_year("Bob", 1990)
+print(bob.age)
+print(bob.name)
