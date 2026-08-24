d6e268d177acc59f389d78c5879b2d0afd3b814f
Michael Bag <michaelbag8@gmail.com>
2026-08-24 08:42:06 +0100

    Implement BetterDate class with from_str method
    Added BetterDate class with a class method to create instances from a date string.

--- class.py
+++ class.py
@@ -108,3 +108,22 @@ print(emp.salary)
 # Give emp a raise of 1500
 emp.give_raise(1500)
 print(emp.salary)
+
+class BetterDate:
+  def __init__(self, year, month, day):
+    self.year, self.month, self.day = year, month, day
+    
+  # Define a class method from_str
+  @classmethod
+  def from_str(cls, datestr):
+    # Split the string at "-"
+    parts = datestr.split("-")
+    year, month, day = int(parts[0]), int(parts[1]), int(parts[2])
+    # Return the class instance
+    return cls(year, month, day)
+
+# Create the xmas object      
+xmas = BetterDate.from_str("2024-12-25")   
+print(xmas.year)
+print(xmas.month)
+print(xmas.day)
