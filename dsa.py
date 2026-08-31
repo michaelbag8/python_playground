8667dc56ac26a686e1601ece69ea44f5b1ce606c
Michael Bag <michaelbag8@gmail.com>
2026-08-31 11:16:56 +0100

    Add dequeue method to queue implementation
    Implement dequeue method for the queue class.

--- dsa.py
+++ dsa.py
@@ -6,3 +6,12 @@ def enqueue(self,data):
     else:
       self.tail.next = new_node
       self.tail = new_node
+
+def dequeue(self):
+    if self.head:
+      current_node = self.head
+      self.head = current_node.next
+      current_node.next = None
+
+      if self.head == None:
+        self.tail = None
