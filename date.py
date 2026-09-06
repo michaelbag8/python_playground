b8054b0329605751b82d8c3cbc038208dad43bb2
michaelbag8 <michaelbag8@gmail.com>
2026-09-06 17:13:06 +0100

    implement python datetime

---
+++ date.py
@@ -0,0 +1,16 @@
+# Import date
+from datetime import date
+
+# Create a date object for May 9th, 2007
+start = date(2007, 5, 9)
+
+# Create a date object for December 13th, 2007
+end = date(2007, 12, 13)
+
+# Subtract the two dates and print the number of days
+print((end - start).days)
+
+
+hurricane_andrew = date(1992, 8, 24)
+
+print(hurricane_andrew.weekday())
