308ddad72ca3ad8229882fd2a6e6f25deec091bc
michaelbag8 <michaelbag8@gmail.com>
2026-09-06 17:37:39 +0100

    Representing date in different format

--- date.py
+++ date.py
@@ -14,3 +14,42 @@ print((end - start).days)
 hurricane_andrew = date(1992, 8, 24)
 
 print(hurricane_andrew.weekday())
+
+
+# A dictionary to count hurricanes per calendar month
+hurricanes_each_month = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6:0,
+		  				 7: 0, 8:0, 9:0, 10:0, 11:0, 12:0}
+
+# Loop over all hurricanes
+for hurricane in florida_hurricane_dates:
+  # Pull out the month
+  month = hurricane.month
+
+  # Increment the count in your dictionary by one
+  hurricanes_each_month[month] += 1
+  
+print(hurricanes_each_month)
+
+# Create a date object
+andrew = date(1992, 8, 26)
+
+# Print the date in the format 'YYYY-MM'
+print(andrew.strftime("%Y-%m"))
+
+# Create a date object
+andrews = date(1992, 8, 26)
+
+# Print the date in the format 'MONTH (YYYY)'
+print(andrews.strftime("%B (%Y)"))
+
+
+john = date(1992, 8, 26)
+
+# Print the date in the format 'YYYY-DDD'
+print(john.strftime("%Y-%d"))
+
+
+zoe = date(1992, 8, 26)
+
+# Print the date in the format 'YYYY-DDD'
+print(zoe.strftime("%Y-%j"))
